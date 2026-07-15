bb86b72e4871cfbd34334828dff2a89519589183
Poojar-git <pooja.r@diggibyte.com>
2026-07-15 16:48:31 +0530

    Modify Products table structure in Alter.sql

--- src/Alter.sql
+++ src/Alter.sql
@@ -1,12 +1,12 @@
 Use OnlineStoreDB;
-/*ALTER TABLE Products
+ALTER TABLE Products
 ADD Brand NVARCHAR(50);
 ALTER TABLE Products
 ADD
 ManufactureDate DATE,
 WarrantyYears INT;
 ALTER table Products
-ALTER COLUMN Brand NVARCHAR(100);*/
+ALTER COLUMN Brand NVARCHAR(100);
 ALTER table Products
 ALTER COLUMN Price 
 
